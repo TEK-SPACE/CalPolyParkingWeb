5dab191c61e9f37a882601f5a51480f658bc2420
Joel Braun <Joelbraun@outlook.com>
2017-06-06 16:23:44 -0700

    askldfalksdf

--- ParkixUI/Controllers/DashboardController.cs
+++ ParkixUI/Controllers/DashboardController.cs
@@ -4,27 +4,91 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Parkix.Shared.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Parkix.Shared.Helpers;
+using Parkix.Shared.Entities.Uaa;
+using System.Net.Http.Headers;
+using Parkix.UI.Models.Dashboard;
 
 namespace Parkix.UI.Controllers
 {
     public class DashboardController : Controller
     {
-        // GET: Dashboard
-        public ActionResult Index()
+        public string ConfigureToken { get; set; }
+        public string ReportToken { get; set; }
+        private static readonly HttpClient configureClient = new HttpClient();
+        private static readonly HttpClient reportClient = new HttpClient();
+
+        public static DashboardModel dashModel = new DashboardModel();
+
+        public string GetOAuthToken(string userName, string secret)
         {
-            return View();
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("client_id", userName),
+                new KeyValuePair<string, string>("grant_type","client_credentials")
+            });
+
+            HttpClient junkClient;
+            string response = "";
+            using (junkClient = new HttpClient())
+            {
+                junkClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(string.Format("{0}:{1}", userName, secret))));
+                response = junkClient.PostAsync("https://a7cc7c64-503e-43df-ab3a-18c97ef60505.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token", content).Result.Content.ReadAsStringAsync().Result;
+            }
+
+            dynamic tokenData = JObject.Parse(response);
+
+            return tokenData["access_token"].Value;
         }
 
-        // GET: Dashboard/Details/5
-        public ActionResult Details(int id)
+        // GET: Dashboard.
+        public ActionResult Index()
         {
-            return View();
+            dashModel.ParkingName = "Cal Poly Parking Infrastructure";
+            ConfigureToken = GetOAuthToken("configure_client", "WMEv8luHI662207td9YFw4SIAuUy9nsM79VKLh4HQOOA7hzFvHTf0t3FcZCkmdtb9aWswGpifS0BPbUqJKvXMtcS2oBG0B9GoIcxf22Jh4J7kmg46fKrGYZVsATfCBdx");
+            ReportToken = GetOAuthToken("report_client", "F8dQA3fOqzdXdZsknaobYKiWIG7xFi71JRmhQMGahIhH09ObiF9nzmTmO0hd71j9NHNJzj1G73KvAO5t9It2sibnB17vMPMlf7fGFlXvIA7PjZDpA1WlOyBUR21RSj1t");
+            var lotsRequest = new HttpRequestMessage(HttpMethod.Get, "https://cp-parkix-configure.run.aws-usw02-pr.ice.predix.io/api/lots");
+            lotsRequest.Headers.Add("authorization", ConfigureToken);
+            var lotResult = configureClient.SendAsync(lotsRequest).Result.Content.ReadAsStringAsync().Result;
+            dashModel.Lots = JArray.Parse(lotResult).ToObject<List<string>>();
+            ViewData["token"] = ConfigureToken;
+
+            return View(dashModel);
+        }
+
+        public IActionResult Sensor(string id)
+        {
+
+            ViewData["id"] = id;
+            //we need to get this data dynamically
+            return View(dashModel);
+        }
+
+        public IActionResult Config(string id)
+        {
+            ViewData["id"] = id;
+            //we need to get this image dynamically. 
+            ViewData["image"] = "../../../images/test23.jpg";
+            return View(dashModel);
         }
 
         // GET: Dashboard/Create
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendConfig([FromBody] string guid, [FromBody] string points)
+        {
+            // POST the data from here to Joe's server
+
+            return RedirectToAction("Config/" + guid);
         }
 
         // POST: Dashboard/Create
---
+++ ParkixUI/Models/Dashboard/DashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkix.UI.Models.Dashboard
+{
+    public class DashboardModel
+    {
+        public List<String> Lots { get; set; }
+        public string ParkingName { get; set; }
+    }
+}
